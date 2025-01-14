6e71f44bb040a7dc69355ce9ed4c0e4ae8594d5d
Edgarsu2024 <edgarito.s.t@gmail.com>
2025-01-14 09:22:16 -0400

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Inicio', component: InicioComponent},
+  {path: 'AcercaDe', component: AcercaDeComponent},
+  {path: '*', component: InicioComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
